18ca7fa6b6f9fbb5b2abbca1d7f0f2e6e1477484
mohammad <you@example.com>
2022-05-24 23:43:14 -0400

    creat succes!

---
+++ src/test/java/jankins/JenkinsSample.java
@@ -0,0 +1,13 @@
+package jankins;
+
+import org.testng.annotations.Test;
+
+public class JenkinsSample {
+
+    @Test
+    public void test_One(){
+        System.out.println("project pass");
+
+    }
+
+}
